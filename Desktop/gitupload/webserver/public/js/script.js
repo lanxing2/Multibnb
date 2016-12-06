7e611828f29439c687f681c3b2477cd5ca99be89
LanXing <lanxing@dyn-129-236-219-250.dyn.columbia.edu>
2016-12-06 11:49:42 -0500

    second commit

---
+++ Desktop/gitupload/webserver/public/js/script.js
@@ -0,0 +1,11 @@
+var ip = IPOFWEBSERVER;
+
+
+var humaniconUrl = ip+"/img/human.png";
+var cariconUrl = ip+"/img/car1small.png";
+
+var destiny = IPOFIMGSERVER;
+
+var postimgurl = destiny+"/postimage";
+var searchimgurl = destiny+"/searchimage";
+var deleteoneimgurl = destiny + "/deleteoneimage"
